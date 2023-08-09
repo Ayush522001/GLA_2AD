83da52622d3ee1bfbbbe2894ae4f775b90194d05
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-08-09 11:15:46 +0530

    Basics

---
+++ Lec_1/Operators.java
@@ -0,0 +1,17 @@
+package Lec_1;
+
+public class Operators {
+    public static void main(String[] args) {
+        int divide=3/8;
+        divide+=3;
+        int mod=3%8;
+        System.out.println(divide);
+        System.out.println(mod);
+        System.out.print("Hello world");
+        System.out.println("5");
+
+        System.out.println(5>=5 || 7==7 || 8>8 || 9==9);
+
+
+    }
+}
